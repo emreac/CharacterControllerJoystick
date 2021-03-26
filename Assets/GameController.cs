528bc16709c10ff71695e37a89b734dfa388915a
Emre Acikgoz <acikgozac@gmail.com>
2021-03-26 22:26:27 +0300

    Controller

---
+++ Assets/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float restartDelay = 5f;
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", restartDelay);
+
+            //Restart The Game
+            Restart();
+        }
+
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/PlayerJoystickController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJoystickController : MonoBehaviour
+{
+
+    //public Rigidbody rb;
+
+    public float speed = 5.0F;
+    public DynamicJoystick moveJoystick;
+    public DynamicJoystick lookJoystick;
+
+    void Update()
+    {
+
+        updateMoveJoystick();
+        updateLookJoystick();
+
+    }
+
+    void updateMoveJoystick()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        float hoz = moveJoystick.Horizontal;
+        float ver = moveJoystick.Vertical;
+        Vector3 direction = new Vector3(hoz, 0, ver).normalized;
+        controller.SimpleMove(direction *speed);
+ 
+    }
+
+    void updateLookJoystick()
+    {
+        float hoz = moveJoystick.Horizontal;
+        float ver = moveJoystick.Vertical;
+        Vector3 direction = new Vector3(hoz, 0, ver).normalized;
+        Vector3 lookAtPosition = transform.position + direction;
+        transform.LookAt(lookAtPosition);
+    }
+    private void FixedUpdate()
+    { /*
+        if (rb.position.y < -4)
+        {
+            FindObjectOfType<GameController>().EndGame();
+        }
+        */
+    }
+}
